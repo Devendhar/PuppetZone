edff20a67fab1cc29b847539b722535023afd62a
root <root@devendhar.example.com>
2017-08-03 08:58:29 +0530

    installed filebeat package

--- modules/elk/manifests/install.pp
+++ modules/elk/manifests/install.pp
@@ -7,6 +7,12 @@ class elk::install inherits elk::params{
 		source => "/opt/${package}",
 	}
 	}
+
+	package { filebeat:
+		provider => rpm,
+		ensure => present,
+		source => '/opt/filebeat-5.5.1-x86_64.rpm',
+	}
 	#package { elasticsearch:
   	#	provider => rpm,
 	#	ensure => present,
